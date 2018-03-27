2c8d26530f0a28ef1db06fac9b14dec13afd9123
Vicente Gonzalez <vgonzalez28@gmail.com>
2018-03-27 13:43:06 -0700

    added figlet, asciify

--- liri.js
+++ liri.js
@@ -1,9 +1,28 @@
+/** Welcome to LIRI BOT!
+ * This a basic app that pulls data from 3 independent sources through a node based request.
+ * requests consist of information from:
+ * -Spotify
+ * -Twitter
+ * -OMDB (IMDB substitute)
+ * Each request is limited by a promp using npm inquirer, which creates a list of what action to be completed.
+ * There are preset restrictions, and some flexibility. The flexibles include:
+ * Spotify: You can either search a song, or leave search blank to return a random song out of 20 choices.
+ * Twitter: Set how many tweets you would like to see by Elon Musk! 
+ * OMDB: Type in what movie you want to know the details about, or leave blank to see a great movie!
+ * (Special) - Click the "I'm feeling lucky" to discover a new song.
+ * 
+ * Enjoy!!
+ */
+
+
 require("dotenv").config();
 var keys = require('./keys.js')
 var Spotify = require('node-spotify-api')
 var Twitter = require('twitter')
 var request = require('request')
 var inq = require('inquirer')
+var figlet = require('figlet')
+var asciify = require('asciify')
 // // console.log(process.env.SPOTIFY_ID)
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -17,37 +36,6 @@ var movietitle
 // standard song search count
 var w = 10
 
-// Initial working function when typing argument in terminal - WORKS
-// switch(input){
-//     case 'my-tweets':
-//     client.get('statuses/user_timeline', {screen_name: 'elonmusk'}, function(error, tweets, response) {
-//         for(var i=0; i<10;i++){
-//         console.log(tweets[i].text);
-//     }
-//      });;
-//     break;
-//     case 'spotify-this-song' :
-//     spotify.search({ type: 'track', query: 'Psycho' }, function(err, data) {
-//         if (err) {
-//           return console.log('Error occurred: ' + err);
-//         }
-
-//       console.log(data); 
-//       });
-//     break;
-//     case 'movie-this':
-//     request('http://www.omdbapi.com/?apikey=trilogy&t=avatar', function(error, response, data){
-//         if(error){ console.log(error)}
-//         console.log((data))
-//     })
-//     break;
-//     case 'do-what-it-says':
-//         console.log("I'm Feeling Lucky");
-//     break;
-//     default:
-//         console.log("must make a choice!")
-
-// }
 
 
 // ------------------------------------------------------------------------------
@@ -107,10 +95,23 @@ var searchconfirm = [
     {
         type: 'input',
         name: 'tweetcount',
-        message: 'How Many do you want to see?(1-30)'
+        message: 'How Many do you want to see?(1-20)'
     }
 
 ]
+// Welcome function is to load Title of program, then runs prompt.
+function welcome(){
+    asciify('Liri Bot', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+        // prompt caller nested within welcome so figlet initializes first
+        promptcaller()
+    });
+}
 
 
 // Execution functions for API when called on in inquirer
@@ -143,7 +144,6 @@ function spotifycall() {
         for (var l = 0; l < w; l++) {
             console.log("Title: " + data.tracks.items[l].name)
             console.log("By: " + data.tracks.items[l].artists[0].name);
-            console.log("Song Preview: "+data.tracks.items[l].href)
             console.log("------------------------")
         }
     });
@@ -155,43 +155,46 @@ function moviecall() {
     })
 }
 
-// initiates inquirer, internal inquirer dependent on selection
-prompt(questions).then(function (r) {
-    selection = r
-    returnvalue = selection.apiaction
-    switch (returnvalue) {
-        case 'Search Elon Musk Tweets':
-        
+function promptcaller(){
+    // initiates inquirer, internal inquirer dependent on selection
+    prompt(questions).then(function (r) {
+        selection = r
+        returnvalue = selection.apiaction
+        switch (returnvalue) {
+            case 'Search Elon Musk Tweets':
+            
             prompt(searchconfirm).then(function (value) {
                 t = (value.tweetcount)
                 twittercall()
             })
             break;
-        case 'Find a Song on Spotify':
+            case 'Find a Song on Spotify':
             prompt(spotifysearch).then(function (song) {
                 if(song.songsearch != ""){
-                spotifysong = song.songsearch
-                spotifycall()
+                    spotifysong = song.songsearch
+                    spotifycall()
                 }else{ 
                     spotifysong = "The Sign Ace of Base"
                     w = 1
                     spotifycall()}
-            })
-            break;
-        case 'Search a Movie':
-            prompt(moviesearch).then(function (movie) {
-                if(movie.titlesearch != ""){
-                movietitle = movie.titlesearch.replace(" ", "+")
-                moviecall()
-                }else{
-                    movietitle = "Mr.+Nobody."
-                    moviecall()
-                }
-            })
-            break;
-        case "I'm Feeling Lucky":
-            random()
-            break;
+                })
+                break;
+                case 'Search a Movie':
+                prompt(moviesearch).then(function (movie) {
+                    if(movie.titlesearch != ""){
+                        movietitle = movie.titlesearch.replace(" ", "+")
+                        moviecall()
+                    }else{
+                        movietitle = "Mr.+Nobody."
+                        moviecall()
+                    }
+                })
+                break;
+                case "I'm Feeling Lucky":
+                random()
+                break;
+            }
+        })
     }
-})
-
+    // Initialize welcome screen and prompts
+    welcome()
